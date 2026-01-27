322b637a2803c83a2f02f052990b619dbcdd76c3
Feroz Ahmed <37993958+khokoon@users.noreply.github.com>
2026-01-27 08:54:21 +0600

    Ajax add

--- resources/views/components/professional-skills.blade.php
+++ resources/views/components/professional-skills.blade.php
@@ -1,7 +1,2 @@
-                        <!-- Skills Section-->
-                        <section>
-                            <!-- Skillset Card-->
-                            <div class="card shadow border-0 rounded-4 mb-5">
-                                <div class="card-body p-5">
-                                    <!-- Professional skills list-->
-                                    <div class="mb-5">
+<!-- Skills Section-->
+<section>
